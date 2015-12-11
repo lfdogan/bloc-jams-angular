da4cb1d9c4343a0c010479f95114b2a95ea36fd3
Lauren Dogan <laurenfdogan@gmail.com>
2015-12-11 11:58:38 -0500

    checkpoint lesson 2 configuration - wire angular to the application

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+//The root Angular module will act as a container for different parts of our application.
+//define a module with angular.module:
+//The first argument passed, blocJams, is the prescribed name of the module. The empty array, passed as the second argument, injects dependencies into an application. For now, there are no dependencies to inject, but we'll cover dependency injection in the next checkpoint.
+angular.module('blocJams', []);
