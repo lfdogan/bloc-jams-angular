25af070d86c63ef8b2f4ce71515e7e040ede144e
Lauren Dogan <laurenfdogan@gmail.com>
2015-12-16 14:08:23 -0500

    complete assignment 5 - add song numbers

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,18 +1,6 @@
  (function() {
      function AlbumCtrl() {
-//         var currentAlbum = fixtures[1];
-//         this.albumArtUrl = currentAlbum.albumArtUrl;
-//         this.albumName = currentAlbum.name;
-//         this.albumArtist = currentAlbum.artist;
-//         this.albumYear = currentAlbum.year;
-//         this.albumLabel = currentAlbum.label;
-         
          this.albumData = fixtures[0];
-         
-         
-//            this.albums = [];
-   //         for (var i=0; i < albumData.songs.length; i++) { 
-    //            this.albums.push(angular.copy(albumData.songs[i])); 
      }
  
      angular
