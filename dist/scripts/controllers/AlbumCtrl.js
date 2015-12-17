60269a80e30324a73a0ab2873e2d137dcf0f0d0d
Lauren Dogan <laurenfdogan@gmail.com>
2015-12-17 12:45:58 -0500

    checkpoint lesson 5 - doesn't work

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,14 @@
  (function() {
-     function AlbumCtrl() {
-         this.albumData = fixtures[0];
+     //function AlbumCtrl() {
+     function AlbumCtrl(Fixtures) {
+         //this.albumData = fixtures[0]; //my variation for assignment 5
+         //this.albumData = albumPicasso; //assignment 5
+         //this.albumData = angular.copy(albumPicasso); // code for lesson 6
+         this.albumData = Fixtures.getAlbum(); //new code for lesson 6: Update AlbumCtrl to use the Fixtures service's getAlbum() method
      }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
+         //.controller('AlbumCtrl', AlbumCtrl);
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]); //lesson 6: We add Fixtures to AlbumCtrl's array of dependencies. Once injected, the service is available for use within the controller.
  })();
--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -1,10 +1,10 @@
  (function() {
      function CollectionCtrl() {
             this.albums = [];
-            for (var i=0; i < fixtures.length; i++) { 
-                this.albums.push(angular.copy(fixtures[i])); //use ALL albums (fixtures array) rather than just one repeated
-            //for (var i=0; i < 12; i++) { 
-                //this.albums.push(angular.copy(albumPicasso));
+            //for (var i=0; i < fixtures.length; i++) { 
+                //this.albums.push(angular.copy(fixtures[i])); //use ALL albums (fixtures array) rather than just one repeated
+            for (var i=0; i < 12; i++) { 
+                this.albums.push(angular.copy(albumPicasso));
                 //angular.copy is one of several global function components on the angular object. We add an albums property and set its value to an empty array. Within the for loop, we use angular.copy to make copies of albumPicasso and push them to the array.
             }
      }
